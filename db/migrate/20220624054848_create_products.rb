366166350ba47b4417bedcafa8ed023e51deba86
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-24 12:59:50 +0530

    added product to database

---
+++ db/migrate/20220624054848_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+  def self.down
+    drop_table :products
+    end
+end
