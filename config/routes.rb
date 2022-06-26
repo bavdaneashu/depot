80bf4ce31aa4211058339ae96770010a77cf2939
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-26 22:32:02 +0530

    store

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
     end
+    resources :support_requests, only: [ :index, :update ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
