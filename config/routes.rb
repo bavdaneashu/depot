5ac7b0f574647af02d7ba364b353ac8da75e368a
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-26 19:05:08 +0530

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  resources :products
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+  resources :products do
+    get :who_bought, on: :member
+  end
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
